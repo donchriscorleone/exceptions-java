21c42bae947480b7ad72d4b70285144e9a12334f
Christopher II Lajom <christopheriilajom@gmail.com>
2021-10-29 09:06:32 +0800

    Added ParseException.

--- ExceptionsDemo.java
+++ ExceptionsDemo.java
@@ -8,6 +8,8 @@ package exceptions;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,11 +20,15 @@ import java.util.logging.Logger;
 public class ExceptionsDemo {
     public static void show() {
         try {
+                        new SimpleDateFormat().parse("");
+
             FileReader reader = new FileReader("text.txt");
             int val = reader.read();
         } catch (FileNotFoundException ex) {
             System.out.println(ex.getMessage());
         } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        } catch (ParseException ex) {
             System.out.println(ex.getMessage());
         }
     }
